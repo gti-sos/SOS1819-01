46d067595289cf4ea632b8b6e51a392819d86e53
berruimar <bernabe-1993@hotmail.com>
2019-03-13 11:14:12 +0000

    arreglado index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 8000;
 
 app.use("/",  express.static(path.join(__dirname, "public")));
 
