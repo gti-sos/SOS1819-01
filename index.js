f98a29d28b394990fddbca1803401896b9c1ff00
sos1819jmbo <pema_12@hotmail.com>
2019-03-19 17:21:38 +0000

    Añadido JoseAPI

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use("/",  express.static(path.join(__dirname, "public")));
 app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/api/v1/major-disasters", require('./api/major-disasters'));
+//app.use("/api/v1/major-disasters", require('./api/major-disasters'));
 //app.use("/api/v1/hurricanes", require('./api/hurricanes'));
-//app.use("/api/v1/testing-of-nuclear-bombs", require('./api/testing-of-nuclear-bombs'));
+app.use("/api/v1/testing-of-nuclear-bombs", require('./api/testing-of-nuclear-bombs'));
 //process.env.NODE_ENV === 'production'
 
 
-//-------JuanAPI
+//-------JuanAPI----------------------------
 
 var hurricanes = [{
     name : "Katrina",
@@ -89,6 +89,121 @@ app.put("/hurricanes/:name", (req,res)=>{
     }
 
 });
+
+//-------JoseAPI---------------------------------------------
+
+var bombs = [{
+    country : "Canada",
+    year : "1959",
+    maxYield : "10000",
+    shot : "5",
+    hob : "0",
+},{
+    country : "Australia",
+    year : "1963",
+    maxYield : "100000",
+    shot : "1",
+    hob : "136",
+},{
+    country : "EEUU",
+    year : "1951",
+    maxYield : "320000",
+    shot : "68",
+    hob : "35",
+}]
+
+// GET /testing-nuclear-bombs
+app.get("/testing-nuclear-bombs",(req,res)=>{
+    res.send(bombs);
+})
+
+
+//POST /testing-nuclear-bombs
+app.post("/testing-nuclear-bombs",(req,res)=>{
+        
+    var newBomb = req.body;
+    
+    bombs.push(newBomb)
+    
+    res.sendStatus(201);
+})
+
+
+//DELETE /testing-nuclear-bombs
+app.delete("/testing-nuclear-bombs", (req,res)=>{
+    
+    bombs =  [];
+
+    res.sendStatus(200);
+});
+
+//GET /testing-nuclear-bombs/EEUU
+
+app.get("/testing-nuclear-bombs/:country", (req,res)=>{
+
+    var country = req.params.country;
+
+    var filteredBombs = bombs.filter((c) =>{
+       return c.country == country; 
+    })
+    
+    if (filteredBombs.length >= 1){
+        res.send(filteredBombs[0]);
+    }else{
+        res.sendStatus(404);
+    }
+
+});
+
+
+//PUT /testing-nuclear-bombs/EEUU
+app.put("/testing-nuclear-bombs/:country", (req,res)=>{
+
+    var country = req.params.country;
+    var updatedBomb = req.body;
+    var found = false;
+
+    var updatedBombs = bombs.map((c) =>{
+    
+        if(c.country == country){
+            found = true;
+            return updatedBomb;
+        }else{
+            return c;            
+        }
+ 
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        bombs = updatedBombs;
+        res.sendStatus(200);
+    }
+
+});
+
+//DELETE /testing-nuclear-bombs/EEUU
+app.delete("/testing-nuclear-bombs/:country", (req,res)=>{
+
+    var country = req.params.country;
+    var found = false;
+
+    var updatedBombs = bombs.filter((c) =>{
+        if(c.country == country)
+            found = true;
+        return c.country != country;
+    });
+    
+    if (found == false){
+        res.sendStatus(404);
+    }else{
+        bombs = updatedBombs;
+        res.sendStatus(200);
+    }
+});
+
+
 
 app.listen(port, () => {
 	console.log("servidor corriendo en puerto " + port);
