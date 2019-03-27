b9a0391433525cabe5bb1d97d403c1f576c71193
sos1819jmbo <pema_12@hotmail.com>
2019-03-27 00:19:39 +0000

    API-Jose

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(morgan('tiny'));
 
 app.use("/api/v1/major-disasters", require('./api/major-disasters'));
 app.use("/api/v1/secure/major-disasters", require('./api/authMiddleware'), require('./api/major-disasters'));
+app.use("/api/v1/secure/testing-of-nuclear-bombs", require('./api/authMiddleware'));
 
 //app.use("/api/v1/hurricanes", require('./api/hurricanes'));
 //app.use("/api/v1/testing-of-nuclear-bombs", require('./api/testing-of-nuclear-bombs'));
@@ -136,6 +137,8 @@ const MongoClient = require("mongodb").MongoClient;
 const uri = "mongodb+srv://pema:pema@sos-wj0yb.mongodb.net/sos1819?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
+var ObjectID = require('mongodb').ObjectID;
+
 var bombs;
 
 client.connect(err => {
@@ -144,9 +147,13 @@ client.connect(err => {
 });
 
 
+app.get("/api/v1/testing-of-nuclear-bombs/docs", (req, res)=>{
+   res.redirect("https://documenter.getpostman.com/view/6919343/S17tQ7ZH%22");
+});
+
 app.get("/api/v1/testing-of-nuclear-bombs/loadInitialData", (req, res) => {
 
-    var bombs1 = [{
+    var bombsAux = [{
         country: "canada",
         year: 1959,
         maxYield: 10000,
@@ -177,16 +184,40 @@ app.get("/api/v1/testing-of-nuclear-bombs/loadInitialData", (req, res) => {
         shot: 50,
         hob: 175,
     }]
-     bombs = bombs1;
-        
-    res.status(200).send(bombs);
+    
+    bombs.countDocuments({},function(err,c){
+        if(c>0){
+            res.sendStatus(409);
+        } else {
+            bombs.insertMany(bombsAux,function(err,r){
+                res.sendStatus(200);
+            });
+        }
+    });
+    
 
 })
 
 // GET /testing-nuclear-bombs
 app.get("/api/v1/testing-of-nuclear-bombs", (req, res) => {
     
-    bombs.find({}).toArray((err,bombsArray)=>{
+    let search = {fields: {}, page: 0, limit: 100};
+
+    for (let key in req.query) {
+        if (["from", "to"].indexOf(key) > -1) {
+            var nCondition = (key === "from") ? "$gt" : "$lt";
+            if (!search.fields.year) 
+                search.fields.year = {};
+            search.fields.year[nCondition] = parseInt(req.query[key]);
+        } else if (["page", "limit"].indexOf(key) > -1)
+            search[key] = parseInt(req.query[key]);
+        else if (["country", "type"].indexOf(key) > -1)
+            search.fields[key] = {"$in": req.query[key]};
+        else 
+            search.fields[key] = req.query[key];
+    }
+    
+    bombs.find(search.fields).limit(search.limit).skip(search.page * search.limit).toArray((err,bombsArray)=>{
         if(err)
             console.log("Error " + err)
             
@@ -201,35 +232,50 @@ app.post("/api/v1/testing-of-nuclear-bombs",(req, res)=>{
     
     var newBomb = req.body;
     
-    console.log(req.body);
+    var keys = ["country","year","maxYield","shot","hob"];
     
-    bombs.insertOne(newBomb);
-    
-    res.sendStatus(201);
-})
+    for (var i = keys.length - 1; i >= 0; i--) {
+        if (!newBomb.hasOwnProperty(keys[i])) {
+            return res.sendStatus(400);
+        }
+    }
+ 
+    bombs.countDocuments(newBomb,function(err,c){
+        if(c>0){
+            res.sendStatus(409);
+        } else {
+            bombs.insertOne(newBomb,function(err,r){
+                res.sendStatus(201);
+            });
+        }
+    });
+   
+});
 
 
 //DELETE /testing-nuclear-bombs
 app.delete("/api/v1/testing-of-nuclear-bombs", (req, res) => {
 
-    bombs.remove();
-
-    res.sendStatus(200);
+    bombs.remove({},function(err,r){
+        res.sendStatus(200);
+    });
+    
 });
 
 //GET /testing-nuclear-bombs/EEUU
 
-app.get("/api/v1/testing-of-nuclear-bombs/:country", (req, res) => {
+app.get("/api/v1/testing-of-nuclear-bombs/:id", (req, res) => {
 
-    var country1 = req.params.country;
+    var idAux = req.params.id;
+    console.log(idAux);
 
-    bombs.find({country : country1 }).toArray((err,bombsArray)=>{
-        if (bombsArray == []) {
+    bombs.findOne({ _id : new ObjectID(idAux) }, function (err, result) {
+        if (!result) {
             res.sendStatus(404);
         }
         else {
-            res.send(bombsArray);
-    }    
+            res.send(result);
+        }
     });
 
    
@@ -238,61 +284,231 @@ app.get("/api/v1/testing-of-nuclear-bombs/:country", (req, res) => {
 
 
 //PUT /testing-nuclear-bombs/EEUU
-app.put("/api/v1/testing-of-nuclear-bombs/:country", (req, res) => {
-
-    var country = req.params.country;
-    var updatedBomb = req.body;
-    var found = false;
-
-    var updatedBombs = bombs.map((c) => {
-
-        if (c.country == country) {
-            found = true;
-            return updatedBomb;
+app.put("/api/v1/testing-of-nuclear-bombs/:id", (req, res) => {
+    
+    if (req.body._id && req.params.id !== req.body._id)
+        return res.sendStatus(400);    
+        
+    var keys = ["country","year","maxYield","shot","hob"];
+    
+    for (var i = keys.length - 1; i >= 0; i--) {
+        if (!req.body.hasOwnProperty(keys[i])) {
+            return res.sendStatus(400);
         }
-        else {
-            return c;
+    }
+    
+
+    delete req.body._id;
+    
+    bombs.updateOne({_id: new ObjectID(req.params.id)},{$set: req.body}, function (err,c) {
+        if(c && c.matchedCount==0){
+          return res.sendStatus(404);  
         }
+        let code = (err) ? 404 : 200;
+        let msg = (err) ? "Not Found" : "OK";
+        res.status(code).json({code: code, msg: msg});
+    })
 
-    });
-
-    if (found == false) {
-        res.sendStatus(404);
-    }
-    else {
-        bombs = updatedBombs;
-        res.sendStatus(200);
-    }
 
 });
 
 //DELETE /testing-nuclear-bombs/EEUU
-app.delete("/api/v1/testing-of-nuclear-bombs/:country", (req, res) => {
+app.delete("/api/v1/testing-of-nuclear-bombs/:id", (req, res) => {
 
-    var country = req.params.country;
-    var found = false;
+    var idAux = req.params.id;
 
-    var updatedBombs = bombs.filter((c) => {
-        if (c.country == country)
-            found = true;
-        return c.country != country;
-    });
-
-    if (found == false) {
-        res.sendStatus(404);
-    }
-    else {
-        bombs = updatedBombs;
+    bombs.remove({_id:new ObjectID(idAux)},function(err,r){
         res.sendStatus(200);
-    }
+    });
 });
 
 //POST /testing-nuclear-bombs/EEUU
-app.post("/api/v1/testing-of-nuclear-bombs/:country", (req, res) => {
+app.post("/api/v1/testing-of-nuclear-bombs/:id", (req, res) => {
     res.sendStatus(405);
 });
 
 app.put("/api/v1/testing-of-nuclear-bombs/", (req, res) => {
+
+    res.sendStatus(405);
+})
+
+app.get("/api/v1/secure/testing-of-nuclear-bombs/loadInitialData", (req, res) => {
+
+    var bombsAux = [{
+        country: "canada",
+        year: 1959,
+        maxYield: 10000,
+        shot: 5,
+        hob: 0,
+    }, {
+        country: "australia",
+        year: 1963,
+        maxYield: 100000,
+        shot: 1,
+        hob: 136,
+    }, {
+        country: "islandia",
+        year: 1958,
+        maxYield: 40000,
+        shot: 5,
+        hob: 0,
+    }, {
+        country: "eeuu",
+        year: 1951,
+        maxYield: 320000,
+        shot: 68,
+        hob: 35,
+    }, {
+        country: "eeuu",
+        year: 1961,
+        maxYield: 40000,
+        shot: 50,
+        hob: 175,
+    }]
+    
+    bombs.countDocuments({},function(err,c){
+        if(c>0){
+            res.sendStatus(409);
+        } else {
+            bombs.insertMany(bombsAux,function(err,r){
+                res.sendStatus(200);
+            });
+        }
+    });
+    
+
+})
+
+// GET /testing-nuclear-bombs
+app.get("/api/v1/secure/testing-of-nuclear-bombs", (req, res) => {
+    
+    let search = {fields: {}, page: 0, limit: 100};
+
+    for (let key in req.query) {
+        if (["from", "to"].indexOf(key) > -1) {
+            var nCondition = (key === "from") ? "$gt" : "$lt";
+            if (!search.fields.year) 
+                search.fields.year = {};
+            search.fields.year[nCondition] = parseInt(req.query[key]);
+        } else if (["page", "limit"].indexOf(key) > -1)
+            search[key] = parseInt(req.query[key]);
+        else if (["country", "type"].indexOf(key) > -1)
+            search.fields[key] = {"$in": req.query[key]};
+        else 
+            search.fields[key] = req.query[key];
+    }
+    
+    bombs.find(search.fields).limit(search.limit).skip(search.page * search.limit).toArray((err,bombsArray)=>{
+        if(err)
+            console.log("Error " + err)
+            
+        res.send(bombsArray);       
+    });
+    
+});
+
+
+//POST /testing-nuclear-bombs
+app.post("/api/v1/secure/testing-of-nuclear-bombs",(req, res)=>{
+    
+    var newBomb = req.body;
+    
+    var keys = ["country","year","maxYield","shot","hob"];
+    
+    for (var i = keys.length - 1; i >= 0; i--) {
+        if (!newBomb.hasOwnProperty(keys[i])) {
+            return res.sendStatus(400);
+        }
+    }
+ 
+    bombs.countDocuments(newBomb,function(err,c){
+        if(c>0){
+            res.sendStatus(409);
+        } else {
+            bombs.insertOne(newBomb,function(err,r){
+                res.sendStatus(201);
+            });
+        }
+    });
+   
+});
+
+
+//DELETE /testing-nuclear-bombs
+app.delete("/api/v1/secure/testing-of-nuclear-bombs", (req, res) => {
+
+    bombs.remove({},function(err,r){
+        res.sendStatus(200);
+    });
+    
+});
+
+//GET /testing-nuclear-bombs/EEUU
+
+app.get("/api/v1/secure/testing-of-nuclear-bombs/:id", (req, res) => {
+
+    var idAux = req.params.id;
+    console.log(idAux);
+
+    bombs.findOne({ _id : new ObjectID(idAux) }, function (err, result) {
+        if (!result) {
+            res.sendStatus(404);
+        }
+        else {
+            res.send(result);
+        }
+    });
+
+   
+
+});
+
+
+//PUT /testing-nuclear-bombs/EEUU
+app.put("/api/v1/secure/testing-of-nuclear-bombs/:id", (req, res) => {
+    
+    if (req.body._id && req.params.id !== req.body._id)
+        return res.sendStatus(400);    
+        
+    var keys = ["country","year","maxYield","shot","hob"];
+    
+    for (var i = keys.length - 1; i >= 0; i--) {
+        if (!req.body.hasOwnProperty(keys[i])) {
+            return res.sendStatus(400);
+        }
+    }
+    
+
+    delete req.body._id;
+    
+    bombs.updateOne({_id: new ObjectID(req.params.id)},{$set: req.body}, function (err,c) {
+        if(c && c.matchedCount==0){
+          return res.sendStatus(404);  
+        }
+        let code = (err) ? 404 : 200;
+        let msg = (err) ? "Not Found" : "OK";
+        res.status(code).json({code: code, msg: msg});
+    })
+
+
+});
+
+//DELETE /testing-nuclear-bombs/EEUU
+app.delete("/api/v1/secure/testing-of-nuclear-bombs/:id", (req, res) => {
+
+    var idAux = req.params.id;
+
+    bombs.remove({_id:new ObjectID(idAux)},function(err,r){
+        res.sendStatus(200);
+    });
+});
+
+//POST /testing-nuclear-bombs/EEUU
+app.post("/api/v1/secure/testing-of-nuclear-bombs/:id", (req, res) => {
+    res.sendStatus(405);
+});
+
+app.put("/api/v1/secure/testing-of-nuclear-bombs/", (req, res) => {
 
     res.sendStatus(405);
 })
