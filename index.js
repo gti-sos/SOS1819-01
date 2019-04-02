82e7784df306cc8656bb1675dc2a00dc3cef44ba
sos1819jmbo <pema_12@hotmail.com>
2019-04-02 13:28:31 +0000

    API modularizda

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
+var bombsAPI = require("./api-testing-of-nuclear-bombs");
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
@@ -14,15 +15,31 @@ app.use(morgan('tiny'));
 
 //direccion remota 
 
+/////////Conexion APIJOSE/////////
+const MongoClient = require("mongodb").MongoClient;
+const uri = "mongodb+srv://pema:pema@sos-wj0yb.mongodb.net/sos1819?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+    var bombs = client.db("sos1819").collection("bombs");
+    bombsAPI.register(app,bombs);
+    console.log("Connected")
+});
+
 mongoose.connect(mongoAddress, {useNewUrlParser: true});
 
-require("./api/testing-of-nuclear-bombs")(app);
+
 require("./api/hurricanes")(app);
 
 
 app.use("/api/v1/major-disasters", require('./api/major-disasters'));
-app.use("/api/v1/secure/testing-of-nuclear-bombs", require('./api/authMiddleware'));
+
 app.use("/api/v1/secure/major-disasters", require('./api/authMiddleware'), require('./api/major-disasters'));
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("Servidor de NodeJS corriendo en", process.env.IP || "localhost", port);
