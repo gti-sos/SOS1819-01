637783c77a5bd46d098277920b17296c505e3c76
berruimar <47628541+berruimar@users.noreply.github.com>
2019-04-09 15:17:40 +0200

    fix

--- public/fe-jjb/juan-fe-controller.js
+++ public/fe-jjb/juan-fe-controller.js
@@ -1,16 +1,60 @@
 /* global angular $scope */
 var app = angular.module("MiniPostmanApp");
-app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
+app.controller("MainCtrl", function ($scope,$http) {
     console.log("MainCtrl initialized!");
-    $scope.url = "http://sos1819-01.herokuapp.com/api/v1/hurricanes";
-                
-    $scope.send = function (){
+    $scope.url = "/api/v1/hurricanes";
+    $scope.body = "";
+    $scope.status = undefined;
+    $scope.get = function (){
         $http.get($scope.url).then(function (response){
-            $scope.data = JSON.stringify(response.data,null,2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
+        }).catch(function (response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
         });
-    $scope.send = function (){
-        $http.get($scope.url).then(function (response){
-            $scope.data = JSON.stringify(response.data,null,2);
+    };
+    $scope.post = function () {
+        var bdy = {};
+        try {
+            bdy = JSON.parse($scope.body);
+        } catch (e) {
+            alert(e);
+        }
+
+        $http.post($scope.url, bdy).then(function (response){
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
+        }).catch(function (response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
         });
-    }
-}]);
+    };
+
+    $scope.put = function () {
+        var bdy = {};
+        try {
+            bdy = JSON.parse($scope.body);
+        } catch (e) {
+            alert(e);
+        }
+
+        $http.put($scope.url, bdy).then(function (response){
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
+        }).catch(function (response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
+        });
+    };
+
+    $scope.delete = function (){
+        $http.delete($scope.url).then(function (response){
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
+        }).catch(function (response) {
+            $scope.data = JSON.stringify(response.data, null, 2);
+            $scope.status = response.status + " " + response.statusText;
+        });
+    };
+});
