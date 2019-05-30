6eeaef6f29d5e2399d327f4f951ecf704fad3947
berruimar <47628541+berruimar@users.noreply.github.com>
2019-05-30 19:04:45 +0200

    asd

--- public/ui/v1/major-disasters/overview.controller.js
+++ public/ui/v1/major-disasters/overview.controller.js
@@ -10,34 +10,190 @@ function buildStatusPopup (data) {
 }
 
 angular.module('SOS1819-app.majorDisastersApp')
-	.controller('overviewCtrl', function ($scope, $location, $q, MajorDisaster, initialData, ngDialog, autoLoad, SocketIO) {
-		
+.controller('overviewCtrl', function ($scope, $location, $q, MajorDisaster, initialData, ngDialog, autoLoad, SocketIO) {
 
+	var searchObj = $location.search();
+	$scope.loading = false;
+	$scope.data = initialData.data || [];
+
+
+		xAxisData = []; //tags
+		seriesData = [];
+
+		for (var i = 0; i < initialData.data.length; i++) {
+			var elm = initialData.data[i];
+			var index = xAxisData.indexOf(elm.year);
+			if (index === -1) {
+				xAxisData.push(elm.year);
+				seriesData.push(1);
+			}
+			else
+				seriesData[index] += 1;
+		}
+
+		function refSort (targetData, refData) {
+		  // Create an array of indices [0, 1, 2, ...N].
+		  var indices = Object.keys(refData);
+
+		  // Sort array of indices according to the reference data.
+		  indices.sort(function(indexA, indexB) {
+		  	if (refData[indexA] < refData[indexB]) {
+		  		return -1;
+		  	} else if (refData[indexA] > refData[indexB]) {
+		  		return 1;
+		  	}
+		  	return 0;
+		  });
+
+		  // Map array of indices to corresponding values of the target array.
+		  return indices.map(function(index) {
+		  	return targetData[index];
+		  });
+		}
+		
+		var seriesDataSorted = refSort(seriesData, xAxisData);
+		xAxisData.sort();
 
 		var myChart = echarts.init(document.getElementById('main'));
+		var option = {
+			title: {
+				text: 'Incidentes por año'
+			},
+			tooltip: {},
+			legend: {
+				data:['Nº de incidentes']
+			},
+			xAxis: {
+				data: xAxisData
+			},
+			yAxis: {},
+			series: [{
+				name: 'Incidentes',
+				type: 'bar',
+				data: seriesDataSorted
+			}]
+		};
+		myChart.setOption(option);
 
-		        // specify chart configuration item and data
-		        var option = {
-		            title: {
-		                text: 'ECharts entry example'
+
+
+
+
+
+
+		Highcharts.chart('main2', {
+
+		    chart: {
+		        type: 'bubble',
+		        plotBorderWidth: 1,
+		        zoomType: 'xy'
+		    },
+
+		    legend: {
+		        enabled: false
+		    },
+
+		    title: {
+		        text: 'Sugar and fat intake per country'
+		    },
+
+		    subtitle: {
+		        text: 'Source: <a href="http://www.euromonitor.com/">Euromonitor</a> and <a href="https://data.oecd.org/">OECD</a>'
+		    },
+
+		    xAxis: {
+		        gridLineWidth: 1,
+		        title: {
+		            text: 'Daily fat intake'
+		        },
+		        labels: {
+		            format: '{value} gr'
+		        },
+		        plotLines: [{
+		            color: 'black',
+		            dashStyle: 'dot',
+		            width: 2,
+		            value: 65,
+		            label: {
+		                rotation: 0,
+		                y: 15,
+		                style: {
+		                    fontStyle: 'italic'
+		                },
+		                text: 'Safe fat intake 65g/day'
 		            },
-		            tooltip: {},
-		            legend: {
-		                data:['Sales']
+		            zIndex: 3
+		        }]
+		    },
+
+		    yAxis: {
+		        startOnTick: false,
+		        endOnTick: false,
+		        title: {
+		            text: 'Daily sugar intake'
+		        },
+		        labels: {
+		            format: '{value} gr'
+		        },
+		        maxPadding: 0.2,
+		        plotLines: [{
+		            color: 'black',
+		            dashStyle: 'dot',
+		            width: 2,
+		            value: 50,
+		            label: {
+		                align: 'right',
+		                style: {
+		                    fontStyle: 'italic'
+		                },
+		                text: 'Safe sugar intake 50g/day',
+		                x: -10
 		            },
-		            xAxis: {
-		                data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
-		            },
-		            yAxis: {},
-		            series: [{
-		                name: 'Sales',
-		                type: 'bar',
-		                data: [5, 20, 36, 10, 10, 20]
-		            }]
-		        };
+		            zIndex: 3
+		        }]
+		    },
 
-		        // use configuration item and data specified to show chart
-		        myChart.setOption(option);
+		    tooltip: {
+		        useHTML: true,
+		        headerFormat: '<table>',
+		        pointFormat: '<tr><th colspan="2"><h3>{point.country}</h3></th></tr>' +
+		            '<tr><th>Fat intake:</th><td>{point.x}g</td></tr>' +
+		            '<tr><th>Sugar intake:</th><td>{point.y}g</td></tr>' +
+		            '<tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>',
+		        footerFormat: '</table>',
+		        followPointer: true
+		    },
+
+		    plotOptions: {
+		        series: {
+		            dataLabels: {
+		                enabled: true,
+		                format: '{point.name}'
+		            }
+		        }
+		    },
+
+		    series: [{
+		        data: [
+		            { x: 95, y: 95, z: 13.8, name: 'BE', country: 'Belgium' },
+		            { x: 86.5, y: 102.9, z: 14.7, name: 'DE', country: 'Germany' },
+		            { x: 80.8, y: 91.5, z: 15.8, name: 'FI', country: 'Finland' },
+		            { x: 80.4, y: 102.5, z: 12, name: 'NL', country: 'Netherlands' },
+		            { x: 80.3, y: 86.1, z: 11.8, name: 'SE', country: 'Sweden' },
+		            { x: 78.4, y: 70.1, z: 16.6, name: 'ES', country: 'Spain' },
+		            { x: 74.2, y: 68.5, z: 14.5, name: 'FR', country: 'France' },
+		            { x: 73.5, y: 83.1, z: 10, name: 'NO', country: 'Norway' },
+		            { x: 71, y: 93.2, z: 24.7, name: 'UK', country: 'United Kingdom' },
+		            { x: 69.2, y: 57.6, z: 10.4, name: 'IT', country: 'Italy' },
+		            { x: 68.6, y: 20, z: 16, name: 'RU', country: 'Russia' },
+		            { x: 65.5, y: 126.4, z: 35.3, name: 'US', country: 'United States' },
+		            { x: 65.4, y: 50.8, z: 28.5, name: 'HU', country: 'Hungary' },
+		            { x: 63.4, y: 51.8, z: 15.4, name: 'PT', country: 'Portugal' },
+		            { x: 64, y: 82.9, z: 31.3, name: 'NZ', country: 'New Zealand' }
+		        ]
+		    }]
+
+		});
 
 
 
@@ -45,9 +201,20 @@ angular.module('SOS1819-app.majorDisastersApp')
 
 
 
-		var searchObj = $location.search();
-		$scope.loading = false;
-		$scope.data = initialData.data || [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		$scope.count = initialData.count;
 		$scope.filter =  {
 			offset: parseInt(searchObj.offset) || 0,
@@ -161,65 +328,65 @@ angular.module('SOS1819-app.majorDisastersApp')
 							ngDialog.open(buildStatusPopup(res));
 						});
 					});
-			}).catch(function (cRes) {
-				reloadTableData(function () {
-					ngDialog.open(buildStatusPopup(cRes));
+				}).catch(function (cRes) {
+					reloadTableData(function () {
+						ngDialog.open(buildStatusPopup(cRes));
+					});
 				});
-			});
-			
-		};
 
-		$scope.modify = function (id) {
-			$location.hash(id);
-			MajorDisaster.v1.get(id).then(function (res) {
-				ngDialog.open({
-					template: '/ui/v1/major-disasters/overviewItem.template.html',
-					controller: "overviewModItemCtrl",
-					data: {
-						operation: 'modify',
-						item: res.data
-					}, preCloseCallback: function () {
+			};
+
+			$scope.modify = function (id) {
+				$location.hash(id);
+				MajorDisaster.v1.get(id).then(function (res) {
+					ngDialog.open({
+						template: '/ui/v1/major-disasters/overviewItem.template.html',
+						controller: "overviewModItemCtrl",
+						data: {
+							operation: 'modify',
+							item: res.data
+						}, preCloseCallback: function () {
+							reloadTableData(function () {
+								$location.hash('');
+							});
+						}
+					});
+				}).catch(function (res) {
+					var p = buildStatusPopup(res);
+					p.preCloseCallback = function () {
 						reloadTableData(function () {
 							$location.hash('');
 						});
-					}
-				});
-			}).catch(function (res) {
-				var p = buildStatusPopup(res);
-				p.preCloseCallback = function () {
-					reloadTableData(function () {
-						$location.hash('');
-					});
-				};
+					};
 					
-				ngDialog.open(p);
-			});
-		};
-		
-		if (autoLoad) $scope.modify(autoLoad);
-		
+					ngDialog.open(p);
+				});
+			};
 
-		function processFilter (obj) {
-			obj.country = (obj.country) ? obj.country.split(',').map(function (e) {if (e) return e.trim(); }) : obj.country;
-			obj.type = (obj.type) ? obj.type.split(',').map(function (e) {if (e) return e.trim(); }) : obj.type;
-			return obj;
-		}
+			if (autoLoad) $scope.modify(autoLoad);
 
-		$scope.navigate = function (index) {
-			index = (index) ? index : 0;
-			reloadTableData(index);
-		};
 
-	})
+			function processFilter (obj) {
+				obj.country = (obj.country) ? obj.country.split(',').map(function (e) {if (e) return e.trim(); }) : obj.country;
+				obj.type = (obj.type) ? obj.type.split(',').map(function (e) {if (e) return e.trim(); }) : obj.type;
+				return obj;
+			}
 
-	.controller('overviewModItemCtrl', function ($scope, MajorDisaster, $injector, ngDialog, $route) {
-		try {
-			$scope.nData = JSON.parse(JSON.stringify($scope.ngDialogData.item));
-			$scope.operation = $scope.ngDialogData.operation;
-		} catch (e) {
-			$scope.nData = $route.current.locals.initialData.data;
-			$scope.operation =$route.current.locals.initialData.operation;
-		}
+			$scope.navigate = function (index) {
+				index = (index) ? index : 0;
+				reloadTableData(index);
+			};
+
+		})
+
+.controller('overviewModItemCtrl', function ($scope, MajorDisaster, $injector, ngDialog, $route) {
+	try {
+		$scope.nData = JSON.parse(JSON.stringify($scope.ngDialogData.item));
+		$scope.operation = $scope.ngDialogData.operation;
+	} catch (e) {
+		$scope.nData = $route.current.locals.initialData.data;
+		$scope.operation =$route.current.locals.initialData.operation;
+	}
 
 		/*
 		$scope.nData = initialData.data || JSON.parse(JSON.stringify($scope.ngDialogData.item));
@@ -255,8 +422,8 @@ angular.module('SOS1819-app.majorDisastersApp')
 			if ($scope.formItem.$valid) {
 				if ($scope.nData.type.length > 0 && $scope.nData.country.length > 0) {
 					var action = ($scope.operation === 'modify') ? 
-						MajorDisaster.v1.update($scope.nData.event, $scope.nData) :
-						MajorDisaster.v1.add($scope.nData);
+					MajorDisaster.v1.update($scope.nData.event, $scope.nData) :
+					MajorDisaster.v1.add($scope.nData);
 					action.then(function (res) {
 						ngDialog.close($scope.ngDialogId);
 						ngDialog.open(buildStatusPopup(res));
@@ -271,7 +438,7 @@ angular.module('SOS1819-app.majorDisastersApp')
 		};
 	})
 
-	.controller('overviewPopupCtrl', function ($scope, ngDialog) {
+.controller('overviewPopupCtrl', function ($scope, ngDialog) {
 		//console.log($scope.ngDialogData);
 		var res = $scope.ngDialogData;
 		$scope.data = (function (res) {
@@ -280,50 +447,50 @@ angular.module('SOS1819-app.majorDisastersApp')
 				case 200:
 				case 201:
 				case 204:
-					data = {msg: "La operación se ha completado con éxito", type: "success"};
-					break;
+				data = {msg: "La operación se ha completado con éxito", type: "success"};
+				break;
 				case 400:
-					data = {msg: "Datos inválidos", type: "warning"};
-					break;
+				data = {msg: "Datos inválidos", type: "warning"};
+				break;
 				case 404:
-					data = {msg: "No se encuentra el recurso", type: "warning"};
-					break;
+				data = {msg: "No se encuentra el recurso", type: "warning"};
+				break;
 				case 405:
-					data = {msg: "Operación no permitida", type: "danger"};
-					break;
+				data = {msg: "Operación no permitida", type: "danger"};
+				break;
 				case 409:
-					data = {msg: "El recurso ya existe", type: "warning"};
-					break;
+				data = {msg: "El recurso ya existe", type: "warning"};
+				break;
 				case 502:
-					data = {msg: "Operación no disponible", type: "warning"};
-					break;
+				data = {msg: "Operación no disponible", type: "warning"};
+				break;
 				default:
-					data = {msg: "La operación se ha completado con éxito", type: "success"};
+				data = {msg: "La operación se ha completado con éxito", type: "success"};
 			}
 			return data;
 		})(res);
 	})
 
-	.controller('overviewLiveGraphCtrl', function ($scope, MajorDisaster, initialData, SocketIO) {
-		var fields = {label: 'type', data: 'country'};
-		$scope.labels = [];
-		$scope.data = [];
+.controller('overviewLiveGraphCtrl', function ($scope, MajorDisaster, initialData, SocketIO) {
+	var fields = {label: 'type', data: 'country'};
+	$scope.labels = [];
+	$scope.data = [];
 
 
-		$scope.init = (function () {
-			for (var i = 0; i < initialData.data.length; i++) {
-				var obj = initialData.data[i];
-				for (var j = 0; j < obj.type.length; j++) {
-					var index = $scope.labels.indexOf(obj.type[j]);
-					if (index === -1) {
-						$scope.labels.push(obj.type[j]);
-						$scope.data.push(obj.country.length);
-					} else {
-						$scope.data[index] += obj.country.length;
-					}
+	$scope.init = (function () {
+		for (var i = 0; i < initialData.data.length; i++) {
+			var obj = initialData.data[i];
+			for (var j = 0; j < obj.type.length; j++) {
+				var index = $scope.labels.indexOf(obj.type[j]);
+				if (index === -1) {
+					$scope.labels.push(obj.type[j]);
+					$scope.data.push(obj.country.length);
+				} else {
+					$scope.data[index] += obj.country.length;
 				}
 			}
-		})();
+		}
+	})();
 		/*
 		setTimeout(function () {
 			$scope.labels = $scope.labels;
