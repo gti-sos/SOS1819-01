19c60e6cb0cbe2d13e0fc1f82af2e08b2b520477
sos1819jmbo <pema_12@hotmail.com>
2019-05-31 08:45:44 +0000

    APIKEY

--- public/ui/v1/testing-of-nuclear-bombs/jose-bo-controller.js
+++ public/ui/v1/testing-of-nuclear-bombs/jose-bo-controller.js
@@ -133,7 +133,7 @@ app.controller("PemaController", function ($scope,$http, $q){
                 'packages':['geochart'],
                 // Note: you will need to get a mapsApiKey for your project.
                 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-                'mapsApiKey': 'AIzaSyDKWisx0-8N-R7YqB2bbzYrjCwwTFBkTZ4'
+                //'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
               });
               google.charts.setOnLoadCallback(drawRegionsMap);
         
