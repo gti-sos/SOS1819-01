7e64f9f1d760ab62b2cb90d2ada4039dbb3c8202
sos1819jmbo <pema_12@hotmail.com>
2019-05-31 10:40:28 +0000

    VZ1

--- public/ui/v1/testing-of-nuclear-bombs/jose-bo-controller.js
+++ public/ui/v1/testing-of-nuclear-bombs/jose-bo-controller.js
@@ -1,41 +1,45 @@
 var app = angular.module("SOS1819-app.Pema");
-app.controller("PemaController", function ($scope,$http, $q){
+app.controller("PemaController", function($scope, $http, $q) {
     var url = "/api/v1/testing-of-nuclear-bombs";
     var url2 = "/api/v2/testing-of-nuclear-bombs";
 
-    $scope.data=[];
-    $scope.body={};
-    $scope.filter={from:null,to:null};
-    $scope.pagination={offset:0,limit:10}
+    $scope.data = [];
+    $scope.body = {};
+    $scope.filter = { from: null, to: null };
+    $scope.pagination = { offset: 0, limit: 100 }
     $scope.count = 0;
-    
-    
-    $scope.get = function(){
+    var dataExt = [];
+    var dataExt1 = [];
+
+    $scope.get = function() {
         var obj = JSON.parse(JSON.stringify($scope.filter));
         //var search = Object.assign(obj, $scope.pagination);
         var search = angular.merge(obj, $scope.pagination);
-        $q.all([$http.get(url,{params:search}), $http.get(url2 + '/count',{params:search})]).then(function(responses){
+        $q.all([$http.get(url, { params: search }), $http.get(url2 + '/count', { params: search }), $http.get("/proxy/youth-unemployment-stats"),
+            $http.get("/proxy/emigrations-by-countries")
+        ]).then(function(responses) {
             $scope.data = responses[0].data;
             $scope.count = Math.ceil(responses[1].data.count / $scope.pagination.limit);
-            
-            
-            
-            
-/////////////////////////////////////////////VISUALIZACION///////////////////////////////////////////////////////////
+            dataExt = responses[2].data;
+            dataExt1 = responses[3].data;
+
+
+            /////////////////////////////////////////////VISUALIZACION///////////////////////////////////////////////////////////
 
             var aux = {};
             var dataGraf = [];
-        
-            for (var i = 0; i<$scope.data.length;i++){
+
+            for (var i = 0; i < $scope.data.length; i++) {
                 var object = $scope.data[i];
                 var exist = aux[object.country];
-                if(exist){
+                if (exist) {
                     aux[object.country] += object.shot;
-                } else {
+                }
+                else {
                     aux[object.country] = object.shot;
                 }
             }
-            
+
             for (var key in aux) {
                 dataGraf.push([key, aux[key]]);
             }
@@ -45,38 +49,43 @@ app.controller("PemaController", function ($scope,$http, $q){
 
             // create an area series and set the data
             var series = chart.area(dataGraf);
-    
+
             // set the chart title
             chart.title("Shot for Countries");
 
             // set the titles of the axes
             chart.xAxis().title("Countries");
             chart.yAxis().title("Shot");
-
+            
+            
+            //Configurar tooltip
+            chart.tooltip().title("Shot for Countries");
+            chart.tooltip().format("Countries: {%categoryName} \n Shot: {%value}");
             // set the container id
             chart.container("migraf");
 
             // initiate drawing the chart
             chart.draw();
-            
-            
+
+
             var aux2 = {};
             var dataGraf2 = [];
-        
-            for (var i = 0; i<$scope.data.length;i++){
+
+            for (var i = 0; i < $scope.data.length; i++) {
                 var object2 = $scope.data[i];
                 var exist = aux2[object.country];
-                if(exist){
+                if (exist) {
                     aux2[object2.country] += object2.maxYield;
-                } else {
+                }
+                else {
                     aux2[object2.country] = object2.maxYield;
                 }
             }
-            
+
             for (var key in aux2) {
-                dataGraf2.push({ name:key , y:aux2[key]});
+                dataGraf2.push({ name: key, y: aux2[key] });
             }
-            
+
             Highcharts.chart('migraf2', {
                 chart: {
                     plotBackgroundColor: null,
@@ -109,135 +118,232 @@ app.controller("PemaController", function ($scope,$http, $q){
                     data: dataGraf2
                 }]
             });
-            
+
             var aux3 = {};
             var dataGraf3 = [];
-            dataGraf3.push(['Country', 'HOB']);
-            
-        
-            for (var i = 0; i<$scope.data.length;i++){
+            dataGraf3.push(['Country', 'NPruebas']);
+
+
+            for (var i = 0; i < $scope.data.length; i++) {
                 var object3 = $scope.data[i];
                 var exist = aux3[object3.country];
-                if(exist){
+                if (exist) {
                     aux3[object3.country] += 1;
-                } else {
+                }
+                else {
                     aux3[object3.country] = 1;
                 }
             }
-            
+
             for (var key in aux3) {
                 dataGraf3.push([key, aux3[key]]);
             }
-            
+
             google.charts.load('current', {
-                'packages':['geochart'],
+                'packages': ['geochart'],
                 // Note: you will need to get a mapsApiKey for your project.
                 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-                //'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-              });
-              google.charts.setOnLoadCallback(drawRegionsMap);
-        
-              function drawRegionsMap() {
+                'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+            });
+            google.charts.setOnLoadCallback(drawRegionsMap);
+
+            function drawRegionsMap() {
                 var data = google.visualization.arrayToDataTable(dataGraf3);
-        
+
                 var options = {};
-        
+
                 var chart = new google.visualization.GeoChart(document.getElementById('migraf3'));
-        
+
                 chart.draw(data, options);
-              }
-        
-        
-//////////////////////////////////////////FINAL VISUALIZACIÓN//////////////////////////////////////////////////        
-        
-        
-        }).catch(function(response){
+            }
+
+            var aux4 = {};
+            var dataGraf4 = [];
+
+            for (var i = 0; i < dataExt.length; i++) {
+                var object4 = dataExt[i];
+                if (object4.year == 2017) {
+                    var exist = aux4[object4.country];
+                    if (!exist) {
+                        aux4[object4.country] = object4.youth_unemployment;
+                    }
+                }
+
+            }
+            for (var key in aux4) {
+                dataGraf4.push([key, aux4[key]]);
+            }
+
+            // create a chart
+            chart = anychart.bar();
+
+            // create a bar series and set the data
+            var series = chart.bar(dataGraf4);
+
+            // set the chart title
+            chart.title("Nivel de paro en ciertos paises.");
+
+            // set the titles of the axes
+            chart.xAxis().title("Paro");
+            chart.yAxis().title("País");
+            
+            //Configurar tooltip
+            chart.tooltip().title("Countries");
+            chart.tooltip().format("Countries: {%categoryName} \n Paro: {%value}");
+            // set the container id
+            chart.container("grafAnd");
+
+            // initiate drawing the chart
+            chart.draw();
+
+            var dataGraf5 = [];
+
+            for (var i = 0; i < dataExt1.length; i++) {
+                var object5 = dataExt1[i];
+                var exist = dataGraf5[object5.country];
+                if (!exist) {
+                    dataGraf5.push([object5.country, object5.totalemigrant])
+                }
+
+            }
+            console.log(dataGraf5);
+            // Set up the chart
+            Highcharts.chart('grafEmi', {
+                chart: {
+                    type: 'pyramid3d',
+                    options3d: {
+                        enabled: true,
+                        alpha: 10,
+                        depth: 50,
+                        viewDistance: 50
+                    }
+                },
+                title: {
+                    text: 'Emigrantes por paises'
+                },
+                plotOptions: {
+                    series: {
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b> ({point.y:,.0f})',
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                            allowOverlap: true,
+                            x: 10,
+                            y: -5
+                        },
+                        width: '60%',
+                        height: '80%',
+                        center: ['50%', '45%']
+                    }
+                },
+                series: [{
+                    name: 'Emigrantes',
+                    data: dataGraf5
+                }]
+            });
+
+            //////////////////////////////////////////FINAL VISUALIZACIÓN//////////////////////////////////////////////////        
+
+
+        }).catch(function(response) {
             window.alert("No se han obtenido los datos.");
         });
     };
-    $scope.navigate = function(index){
+    $scope.navigate = function(index) {
         $scope.pagination.offset = index;
         $scope.get();
     };
     $scope.get();
-    $scope.delete = function(name){
-        $http.delete(url + "/" + name).then(function (response) {
+    $scope.delete = function(name) {
+        $http.delete(url + "/" + name).then(function(response) {
             $scope.get();
             window.alert("La operacion ha sido realizada con exito.");
-        }).catch(function(response){
-           window.alert("No se ha encontrado el objeto a borrar.");
+        }).catch(function(response) {
+            window.alert("No se ha encontrado el objeto a borrar.");
         });
     };
-    
-    $scope.post = function(){
-        $http.post(url,$scope.body).then(function (response) {
+
+    $scope.post = function() {
+        $http.post(url, $scope.body).then(function(response) {
             $scope.get();
             window.alert("La operacion ha sido realizada con exito.");
-            $scope.body={};
-        }).catch(function(response){
-            if(response.status===409){
-                window.alert("Ya existe el objeto."+ $scope.body.name);
-            } else if (response.status===400){
+            $scope.body = {};
+        }).catch(function(response) {
+            if (response.status === 409) {
+                window.alert("Ya existe el objeto." + $scope.body.name);
+            }
+            else if (response.status === 400) {
                 window.alert("Faltan campos por completar.");
-            } else {
+            }
+            else {
                 window.alert("No se ha podido crear.");
             }
         });
     };
-    $scope.put = function(){
-        $http.put(url + "/"+ $scope.body.name,$scope.body).then(function (response) { 
+    $scope.put = function() {
+        $http.put(url + "/" + $scope.body.name, $scope.body).then(function(response) {
             $scope.get();
             window.alert("La operacion ha sido realizada con exito.");
-            $scope.body={};
-        }).catch(function(response){
-            if(response.status===404){
-                window.alert("No existe el objeto."+ $scope.body.name);
-            } else if (response.status===400){
+            $scope.body = {};
+        }).catch(function(response) {
+            if (response.status === 404) {
+                window.alert("No existe el objeto." + $scope.body.name);
+            }
+            else if (response.status === 400) {
                 window.alert("Faltan campos por completar.");
-            } else {
+            }
+            else {
                 window.alert("No se ha podido modificar.");
             }
         });
     };
-    $scope.deleteAll = function(){
-        $http.delete(url).then(function (response){
+    $scope.deleteAll = function() {
+        $http.delete(url).then(function(response) {
             $scope.get();
             window.alert("La operacion ha sido realizada con exito.");
-        }).catch(function(response){
+        }).catch(function(response) {
             window.alert("No se ha completado la operacion.");
         });
-        
+
     };
-    
+
 });
 
 
 
 
 
-app.controller("PemaEditController", function ($scope,$http, $q, initialData,$location){
+app.controller("PemaEditController", function($scope, $http, $q, initialData, $location) {
     console.log(initialData);
     var url = "/api/v1/testing-of-nuclear-bombs";
     var url2 = "/api/v2/testing-of-nuclear-bombs";
-    
-    $scope.body={name:initialData.name,country:initialData.country,year:parseFloat(initialData.year),maxYield:parseFloat(initialData.maxYield),
-        shot:parseFloat(initialData.shot),hob:parseFloat(initialData.hob)};
-    
-    $scope.put = function(){
-        $http.put(url + "/"+ $scope.body.name,$scope.body).then(function (response) { 
+
+    $scope.body = {
+        name: initialData.name,
+        country: initialData.country,
+        year: parseFloat(initialData.year),
+        maxYield: parseFloat(initialData.maxYield),
+        shot: parseFloat(initialData.shot),
+        hob: parseFloat(initialData.hob)
+    };
+
+    $scope.put = function() {
+        $http.put(url + "/" + $scope.body.name, $scope.body).then(function(response) {
             //$scope.get();
             window.alert("La operacion ha sido realizada con exito.");
-            $scope.body={};
+            $scope.body = {};
             $location.url("/testing-of-nuclear-bombs");
-        }).catch(function(response){
-            if(response.status===404){
-                window.alert("No existe el objeto."+ $scope.body.name);
-            } else if (response.status===400){
+        }).catch(function(response) {
+            if (response.status === 404) {
+                window.alert("No existe el objeto." + $scope.body.name);
+            }
+            else if (response.status === 400) {
                 window.alert("Faltan campos por completar.");
-            } else {
+            }
+            else {
                 window.alert("No se ha podido modificar.");
             }
         });
     };
-    
+
 });
