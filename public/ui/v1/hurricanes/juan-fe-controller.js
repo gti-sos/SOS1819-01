738ab13840dcf4570867d11832ed4ed7a3196284
sos1819jjb <jjballina66@gmail.com>
2019-05-31 10:43:55 +0000

    yay

--- public/ui/v1/hurricanes/juan-fe-controller.js
+++ public/ui/v1/hurricanes/juan-fe-controller.js
@@ -81,6 +81,7 @@ app.controller("juan-fe-controller", function ($scope, $http, $q) {
         $q.all([$http.get($scope.url,{params:search}), $http.get($scope.url2 + '/count',{params:search})]).then(function(responses){
             $scope.hurricanes = responses[0].data;
             cuentadanos();
+            fium();
             $scope.count = Math.ceil(responses[1].data.count / $scope.pagination.limit);
             if (cb) cb();
         }).catch(function(response){
@@ -242,7 +243,172 @@ app.controller("juan-fe-controller", function ($scope, $http, $q) {
                 width: "100%" //default: "100%"
             }            
         ) );
-    };
+    }
+    
+    function fium(){
+        var v=0;
+        var i=0;
+        var o=0;
+        
+        $scope.hurricanes.forEach(function(e){
+            v= v +parseInt(e.speed);
+            i++;
+        });
+        console.log("speed="+v);
+        v=v/i;
+        console.log("i="+i);
+        console.log("medium speed="+v);
+        i=0;
+        
+        Highcharts.chart('fium', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+
+    title: {
+        text: 'speed'
+    },
+
+    pane: {
+        startAngle: 0,
+        endAngle: 360,
+        background: [{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                ]
+            },
+            borderWidth: 0,
+            outerRadius: '109%'
+        }, {
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                ]
+            },
+            borderWidth: 1,
+            outerRadius: '107%'
+        }, {
+            // default background
+        }, {
+            backgroundColor: '#DDD',
+            borderWidth: 0,
+            outerRadius: '105%',
+            innerRadius: '103%'
+        }]
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 300,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
+        },
+        title: {
+            text: 'km/h'
+        },
+        plotBands: [{
+            from: 0,
+            to: 200,
+            color: '#55BF3B' // green
+        }, {
+            from: 200,
+            to: 250,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 250,
+            to: 300,
+            color: '#DF5353' // red
+        }]
+    },
+
+    
+    series: [{
+        name: 'speed',
+        data: [v],
+        tooltip: {
+            valueSuffix: ' km/h '
+        }
+    }]
+
+},
+// Add some life
+function (chart) {
+    if (!chart.renderer.forExport) {
+        setInterval(function () {
+            
+            console.log("Value of o = ", o)    
+            if(o==0){
+            var point = chart.series[0].points[0],
+                newVal,
+                inc = -5;
+
+            newVal = point.y + inc;
+            console.log("entra1");
+            point.update(newVal);
+            
+            o++;    
+            }else if(o==1){
+                var point = chart.series[0].points[0],
+                newVal,
+                inc = 5;
+
+            newVal = point.y + inc;
+            
+            console.log("entra2"+o);
+            point.update(newVal);
+            
+            o++;
+            }else if(o==2){
+                var point = chart.series[0].points[0],
+                newVal,
+                inc = 5;
+
+            newVal = point.y + inc;
+
+            point.update(newVal);
+            console.log("entra3"+o);
+            o++;
+            }else{
+                var point = chart.series[0].points[0],
+                newVal,
+                inc = -5;
+
+            newVal = point.y + inc;
+
+            point.update(newVal);
+            
+            o=0;}
+
+        }, 100);
+            
+    }
+    }
+);
+    }
 
     
 })
