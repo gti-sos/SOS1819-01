439c1637ff3c51af8b70a8848ad86737124985b2
berruimar <47628541+berruimar@users.noreply.github.com>
2019-06-02 22:06:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.use(morgan('dev'));
 //app.set('views', [path.join(__dirname, 'public')]);
 //app.set('view engine', 'pug');
 
+console.log(__dirname)
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/major-disasters", require('./majorDisastersRoutes.js'));
 app.use("/proxy", require('./externalRoutes.js'));
