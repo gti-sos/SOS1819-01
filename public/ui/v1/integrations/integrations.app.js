040b1f86f33305247cc8d2b739fbbc14b27b82c5
berruimar <47628541+berruimar@users.noreply.github.com>
2019-06-03 21:56:43 +0200

    unified view

--- public/ui/v1/integrations/integrations.app.js
+++ public/ui/v1/integrations/integrations.app.js
@@ -6,31 +6,35 @@ angular.module('SOS1819-app.integrations', ['ngRoute', 'SOS1819-app', 'SOS1819-a
 		reloadOnSearch: true,
 		templateUrl: '/ui/v1/integrations/integrations.template.html',
 		resolve: {
-			initialData: function ($http, MajorDisaster, Hurricanes, TestingOfNuclearBombs, PollutionStats, SportsCenters, WeatherStats, DonaldTrump, $location, $q) {
+			initialData: function ($http, MajorDisaster, Hurricanes, Dogs, Advice, TestingOfNuclearBombs, PollutionStats, SportsCenters, WeatherStats, DonaldTrump, $location, $q) {
 				var initialData = {};
 				var majorDisastersPromises = [
-					MajorDisaster.v1.list({}), 
-					MajorDisaster.v2.count({}),
-					PollutionStats.list({}),
-					SportsCenters.list({}),
-					WeatherStats.list({}),
-					DonaldTrump.random(),
-					DonaldTrump.random(),
-					DonaldTrump.random(),
-					Dogs.list(),
-					Advice.list()
+				MajorDisaster.v1.list({}), 
+				MajorDisaster.v2.count({}),
+				PollutionStats.list({}),
+				SportsCenters.list({}),
+				WeatherStats.list({}),
+				DonaldTrump.random(),
+				DonaldTrump.random(),
+				DonaldTrump.random(),
+				Dogs.list(),
+				Advice.list()
 				];
 
 				var hurricanesPromises = [
-					Hurricanes.get(),
-					$http.get("/proxy/country-stats"),
-            		$http.get("/proxy/computers-attacks-stats"),
-            		$http.get("/proxy/poke"),
-            		$http.get("/proxy/got")
+				Hurricanes.get(),
+				$http.get("/proxy/country-stats"),
+				$http.get("/proxy/computers-attacks-stats"),
+				$http.get("/proxy/poke"),
+				$http.get("/proxy/got")
 				];
 				
 				var bombsPromises = [
-					TestingOfNuclearBombs.get()
+				TestingOfNuclearBombs.get(),
+				$http.get("/proxy/youth-unemployment-stats"),
+				$http.get("/proxy/emigrations-by-countries"),
+				$http.get("/proxy/marcas-vehiculos"), 
+				$http.get("/proxy/albums")
 				];
 				
 				initialData.disasters = $q.all(majorDisastersPromises).then(function (res) {
@@ -51,7 +55,7 @@ angular.module('SOS1819-app.integrations', ['ngRoute', 'SOS1819-app', 'SOS1819-a
 				}).catch(function (res) {
 					console.log('integrations.app.js majorDisastersInitialData exception on resolve', res);
 					return {
-						data: [], ext1: [], ext2: [], ext3: [], ext4: []
+						data: [], ext1: [], ext2: [], ext3: [], ext4: [], count: 0
 					};
 				});
 
@@ -62,7 +66,7 @@ angular.module('SOS1819-app.integrations', ['ngRoute', 'SOS1819-app', 'SOS1819-a
 						ext2: res[2].data, 
 						ext3: res[3].data,
 						ext4: res[4].data
-						
+
 					};
 				}).catch(function (res) {
 					console.log('integrations.app.js hurricanes exception on resolve', res);
@@ -71,7 +75,11 @@ angular.module('SOS1819-app.integrations', ['ngRoute', 'SOS1819-app', 'SOS1819-a
 
 				initialData.bombs = $q.all(bombsPromises).then(function (res) {
 					return {
-						data: res[0].data
+						data: res[0].data,
+						ext1: res[1].data,
+						ext2: res[2].data,
+						ext3: res[3].data,
+						ext4: res[4].data
 					};
 				}).catch(function (res) {
 					console.log('integrations.app.js bombs exception on resolve', res);
@@ -82,4 +90,4 @@ angular.module('SOS1819-app.integrations', ['ngRoute', 'SOS1819-app', 'SOS1819-a
 			}
 		}
 	});
-});
+})
