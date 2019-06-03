4d22c89358410a519f6ac84b6e4ff38e1a6f0c01
berruimar <47628541+berruimar@users.noreply.github.com>
2019-06-03 12:06:14 +0200

    replaced echarts

--- public/ui/v1/integrations/integrations.controller.js
+++ public/ui/v1/integrations/integrations.controller.js
@@ -179,6 +179,7 @@ angular.module('SOS1819-app.integrations')
 		}
 		function drawRegionsMap() {
 			var data = google.visualization.arrayToDataTable(labels);
+			console.log(data)
 
 			var options = {};
 
@@ -188,6 +189,86 @@ angular.module('SOS1819-app.integrations')
 		}
 	}
 	
+	function generateLineChart(containerId, ownData, extData) {
+		console.log(extData);
+		var data1 = [];
+		/*
+		var data = extData.map(function (e, i) {
+			return [
+				data1.push({
+					name: ''
+				})
+			];
+		});
+
+*/
+
+		Highcharts.chart(containerId, {
+
+		  title: {
+		    text: 'Solar Employment Growth by Sector, 2010-2016'
+		  },
+
+		  subtitle: {
+		    text: 'Source: thesolarfoundation.com'
+		  },
+
+		  yAxis: {
+		    title: {
+		      text: 'Number of Employees'
+		    }
+		  },
+		  legend: {
+		    layout: 'vertical',
+		    align: 'right',
+		    verticalAlign: 'middle'
+		  },
+
+		  plotOptions: {
+		    series: {
+		      label: {
+		        connectorAllowed: false
+		      },
+		      pointStart: 2010
+		    }
+		  },
+
+		  series: [{
+		    name: 'Installation',
+		    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+		  }, {
+		    name: 'Manufacturing',
+		    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+		  }, {
+		    name: 'Sales & Distribution',
+		    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+		  }, {
+		    name: 'Project Development',
+		    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+		  }, {
+		    name: 'Other',
+		    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+		  }],
+
+		  responsive: {
+		    rules: [{
+		      condition: {
+		        maxWidth: 500
+		      },
+		      chartOptions: {
+		        legend: {
+		          layout: 'horizontal',
+		          align: 'center',
+		          verticalAlign: 'bottom'
+		        }
+		      }
+		    }]
+		  }
+
+		});
+
+	}
+
 	function generateAreaChart (containerId, ownData, extData) {
 		var myChart2 = echarts.init(document.getElementById(containerId));
 		var xAxisData = []; 
@@ -614,7 +695,7 @@ angular.module('SOS1819-app.integrations')
 			geocharts: new graph(generateGeoCharts, 'main3', initialData.disasters.data)
 		},
 		integrations: {
-			ext1: new graph(generateAreaChart, 'main4', initialData.disasters.data, initialData.disasters.ext1),
+			ext1: new graph(generateLineChart, 'main4', initialData.disasters.data, initialData.disasters.ext1),
 			ext2: new graph(generateHeatmap, 'main5', initialData.disasters.data, initialData.disasters.ext2),
 			ext3: new graph(generateRadar, 'main6', initialData.disasters.data, initialData.disasters.ext3.norms),
 			ext4: new graph(generateGraph, 'main7', initialData.disasters.data.slice(0, 8), initialData.disasters.ext4),
